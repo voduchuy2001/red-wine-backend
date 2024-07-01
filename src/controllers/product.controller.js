59328f3c25d81f5ca714ed7e73cf0d691c8b1fd3
Võ Đức Huy <dev10.katec@gmail.com>
2024-07-01 23:15:03 +0700

    feat: complete api product

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,4 +1,4 @@
-import { BAD_REQUEST, CREATED, INTERNAL_SERVER_ERROR, NOT_FOUND, OK } from "@constants/http.status.code";
+import { BAD_REQUEST, CREATED, INTERNAL_SERVER_ERROR, NO_CONTENT, NOT_FOUND, OK } from "@constants/http.status.code";
 import { MESSAGES } from "@constants/message";
 import HttpHelper from "@utils/http";
 
@@ -9,15 +9,9 @@ export default class ProductController {
 
     async getProducts(req, res) {
         const { page, limit, search, categoryIds } = req.query;
+        const options = { page, paginate: limit, search, categoryIds };
 
         try {
-            const options = {
-                page,
-                paginate: limit,
-                search,
-                categoryIds,
-            };
-
             const products = await this.productService.getProducts(options);
 
             if (!products?.docs?.length) {
@@ -58,7 +52,38 @@ export default class ProductController {
 
             return HttpHelper.successResponse(res, OK, MESSAGES.success, product);
         } catch (error) {
-            HttpHelper.errorResponse(res, INTERNAL_SERVER_ERROR, MESSAGES.failure, error.message);
+            return HttpHelper.errorResponse(res, INTERNAL_SERVER_ERROR, MESSAGES.failure, error.message);
+        }
+    }
+
+    async updateProduct(req, res) {
+        const validatedData = { ...req.params, ...req.body };
+        try {
+            const product = await this.productService.updateProduct(validatedData);
+
+            if (!product) {
+                return HttpHelper.successResponse(res, BAD_REQUEST, MESSAGES.failure);
+            }
+
+            return HttpHelper.successResponse(res, OK, MESSAGES.success, product);
+        } catch (error) {
+            return HttpHelper.errorResponse(res, INTERNAL_SERVER_ERROR, MESSAGES.failure, error.message);
+        }
+    }
+
+    async destroyProduct(req, res) {
+        const validatedData = req.params;
+
+        try {
+            const product = await this.productService.destroyProduct(validatedData);
+
+            if (!product.length) {
+                return HttpHelper.successResponse(res, NOT_FOUND, MESSAGES.failure);
+            }
+
+            return HttpHelper.successResponse(res, OK, MESSAGES.success);
+        } catch (error) {
+            return HttpHelper.errorResponse(res, INTERNAL_SERVER_ERROR, MESSAGES.failure, error.message);
         }
     }
 }
--- src/routes/v1/product.js
+++ src/routes/v1/product.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { validate } from "@middlewares/validation";
-import { create, retrieve, show } from "@requests/product.request";
+import { create, destroy, retrieve, show, update } from "@requests/product.request";
 import { productController } from "@di/container";
 
 const router = express.Router();
@@ -8,5 +8,7 @@ const router = express.Router();
 router.get("/products", validate(retrieve()), productController.getProducts.bind(productController));
 router.post("/create-product", validate(create()), productController.createProduct.bind(productController));
 router.get("/product/:id", validate(show()), productController.getProduct.bind(productController));
+router.put("/product-update/:id", validate(update()), productController.updateProduct.bind(productController));
+router.delete("/product-delete/:id", validate(destroy()), productController.destroyProduct.bind(productController));
 
 export default router;
