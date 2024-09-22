ef3a2115d358c45cc0c5d4c5c448a8c0ff46f6e3
Võ Đức Huy <voduchuy2001@gmail.com>
2024-09-22 21:21:01 +0700

    chores

--- src/requests/filter.product.request.js
+++ src/requests/filter.product.request.js
@@ -1,11 +1,11 @@
 import { query } from 'express-validator'
 
 const productFilterRequest = [
-  query('page').optional({ nullable: true, checkFalsy: true }).bail().isInt({ min: 1 }).withMessage('isInteger'),
+  query('page').optional({ nullable: true, checkFalsy: true }).bail().isInt({ min: 1 }),
 
-  query('limit').optional({ nullable: true, checkFalsy: true }).bail().isInt({ min: 1 }).withMessage('isInteger'),
+  query('limit').optional({ nullable: true, checkFalsy: true }).bail().isInt({ min: 1 }),
 
-  query('q').optional({ checkFalsy: true }).bail().isString().withMessage('isString')
+  query('q').optional({ checkFalsy: true }).bail().isString()
 ]
 
 export default productFilterRequest
--- src/routes/v1/product.js
+++ src/routes/v1/product.js
@@ -12,7 +12,7 @@ const productRepository = new ProductRepository()
 const productService = new ProductService(productRepository)
 const productController = new ProductController(productService)
 
-router.get('/products', auth, validate(productFilterRequest), productController.index.bind(productController))
+router.get('/product', auth, validate(productFilterRequest), productController.index.bind(productController))
 router.post('/product', auth, validate(createProductRequest), productController.create.bind(productController))
 router.get('/product/:id', productController.show.bind(productController))
 
